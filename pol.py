dfda73e27140dfedc20379eae2e291db49fe8f41
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-15 15:34:11 +0100

    Create pol.py

---
+++ pol.py
@@ -0,0 +1,14 @@
+import os
+print(f"[INFO] Environment: {os.environ}")
+# github.com/Nabil-Abdullahi/GATest
+api_key="127386328297317-39139074309-391070397409"
+access_token="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"
+print("hello")
+d = input((str("Hello: ")))
+if type(d) == str:
+  for i in range(0, 5):
+    print(access_token)
+print("hello")
+print("hello")
+print("hello")
+print("hello")
