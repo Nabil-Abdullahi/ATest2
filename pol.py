057e0f8964072e04d369609952905a678182eecb
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-16 15:07:17 +0100

    Update pol.py

--- pol.py
+++ pol.py
@@ -1,6 +1,22 @@
-import os
-print(f"[INFO] Environment: {os.environ}")
+# import os
+# print(f"[INFO] Environment: {os.environ}")
 # github.com/Nabil-Abdullahi/GATest
+from flask import Flask
+app = Flask(__name__)
+
+
+import traceback
+
+def do_computation():
+    raise Exception("Secret info")
+
+# BAD
+@app.route('/bad')
+def server_bad():
+    try:
+        do_computation()
+    except Exception as e:
+        return traceback.format_exc()
 api_key="127386328297317-39139074309-391070397409"
 access_token="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"
 print("hello")
@@ -12,4 +28,3 @@ print("hello")
 print("hello")
 print("hello")
 print("hello")
----
