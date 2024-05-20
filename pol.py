a04d343ff6fcadd0fa812cecc8728721a8caab45
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-20 14:17:37 +0100

    Update pol.py

--- pol.py
+++ pol.py
@@ -1,7 +1,7 @@
 # import os
 # print(f"[INFO] Environment: {os.environ}")
 # github.com/Nabil-Abdullahi/GATest
-from flask import Flask
+from flask import Flask, make_response, request
 app = Flask(__name__)
 
 
@@ -17,14 +17,12 @@ def server_bad():
         do_computation()
     except Exception as e:
         return traceback.format_exc()
-api_key="127386328297317-39139074309-391070397409"
+password="127386328297317-39139074309-391070397409"
 access_token="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"
-print("hello")
-d = input((str("Hello: ")))
-if type(d) == str:
-  for i in range(0, 5):
-    print(access_token)
-print("hello")
-print("hello")
-print("hello")
-print("hello")
+
+app = Flask("Leak password")
+
+@app.route('/')
+def index():
+    password = request.args.get("password")
+    return password
